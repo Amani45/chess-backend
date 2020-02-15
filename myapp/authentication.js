83f8d52473a04f5efb6de08a5eea8f9415561b0f
Amani <am.m4@hotmail.com>
2020-02-15 14:09:02 +0300

    adding otp validation

--- myapp/authentication.js
+++ myapp/authentication.js
@@ -1,9 +1,44 @@
 
-function generateOTP(){
+function generateOTP() {
     return Math.floor(1000 + Math.random() * 9000);
+}
+
+function validateSequence(providedSequence, userChessCode, userChessSize) {
+
+    // Logic for determing the right sequecne 
+    // sampleProvidedSequence = [
+    //     { chessaman: { x: 0, y: 0 }, sequecne: ["x": 0, "y": 1}, { "x": 2, "y": 2 }]},
+    //     { chessaman: { x: 0, y: 1 }, sequecne: ["x": 0, "y": 0}, { "x": 2, "y": 2 } ]}
+    // ]
+
+    // smapleUserChessSize = 4
+    
+    // sampleUserChessCode= [
+    //     {
+    //         "x" : 0.0, 
+    //         "y" : 0.0, 
+    //         "chessman" : "queen", 
+    //         "order" : 1.0
+    //     }, 
+    //     {
+    //         "x" : 2.0, 
+    //         "y" : 2.0, 
+    //         "chessman" : "bishop", 
+    //         "order" : 2.0
+    //     }
+    // ]
+    
+
+
+random = Math.floor(Math.random() * 10);
+
+if (random > 5)
+    return true
+else return false
+    
 }
 
 
 module.exports = {
-    generateOTP
+    generateOTP, validateSequence
 }
--- myapp/routes/api.js
+++ myapp/routes/api.js
@@ -6,11 +6,14 @@ var auth = require('../authentication');
 
 const dbName = "chess"
 const colUsers = "Users"
+const colOtps = "Otps"
 
 
 const USERS = "/users"
 const ADD_USER = USERS + "/add"
 const GET_USER_CHESSMEN = USERS + "/get/:userName"
+const VALIDATE_USER_CHESS_CODE = USERS + "/validate/sequence"
+const VALIDATE_USER_OTP_CODE = USERS + "/validate/otp"
 
 
 /**
@@ -57,24 +60,91 @@ router.get(GET_USER_CHESSMEN, function (req, res, next) {
   const userName = req.params.userName
   db.initialize(dbName, colUsers, (collection) => {
 
-    // collection.find().toArray(function (err, items) {
-    //   if (err) {
-    //     res.json({ id: "Error in database query", error: err });
-    //   }
-    // });
-    collection.find({ userName: userName })
-    .project({ userName: 1, chessSize: 1, 'chessCode.x': 1, 'chessCode.y': 1 })
-    .toArray(function (err, item) {
-      console.log("ITEM:", item)
-      if (err) {
-        res.json({ id: "Error in database query", error: err });
-      }
-      res.json(item)
 
-    })
+    collection.find({ userName: userName })
+      .project({ userName: 1, chessSize: 1, 'chessCode.x': 1, 'chessCode.y': 1 })
+      .toArray(function (err, item) {
+        console.log("ITEM:", item)
+        if (err) {
+          res.json({ id: "Error in database query", error: err });
+        }
+        res.json(item)
+
+      })
   }, (err) => {
     res.json({ id: "Error in database connection", error: err });
   })
 });
+
+// validate user chess code
+router.post(VALIDATE_USER_CHESS_CODE, function (req, res, next) {
+
+  const userName = req.body.userName
+  const sequenceCode = req.body.sequenceCode
+
+  db.initialize(dbName, colUsers, (collection) => {
+
+
+    collection.findOne({ userName: userName }, function (err, user) {
+      console.log("ITEM:", user)
+      if (err) {
+        res.json({ id: "Error in database query", error: err });
+      }
+
+      if (auth.validateSequence(sequenceCode, user.chessCode, user.chessSize)) {
+        otp = auth.generateOTP()
+        db.initialize(dbName, colOtps, (collectionOtp) => {
+
+          collectionOtp.insert({ userName: user.userName, otp: otp }, function (err, ids) {
+            if (err) {
+              res.json({ id: "Error in database query", error: err });
+            }
+            sms.sendSMS(user.phone, otp)
+            res.json({ success: true, message: "OTP SMS sent" })
+          })
+
+        }, (err) => {
+          res.json({ id: "Error in database connection", error: err });
+        })
+
+      } else {
+        res.json({ success: false, message: "Chess Sequence is not valid" })
+      }
+
+    })
+
+
+
+  }, (err) => {
+    res.json({ id: "Error in database connection", error: err });
+  })
+});
+
+
+// validate the OTP
+router.post(VALIDATE_USER_OTP_CODE, function (req, res, next) {
+
+  const userName = req.body.userName
+  const otpCode = req.body.otpCode
+
+  db.initialize(dbName, colOtps, (collection) => {
+
+    collection.findOne({ userName: userName }, function (err, otpDocument) {
+      if (err) {
+        res.json({ id: "Error in database query", error: err });
+      }
+
+      if(otpDocument.otp == otpCode){
+        res.json({ success: true, message: "OTP SMS is valid" })
+      }else {
+        res.json({ success: false, message: "OTP SMS is not valid" }) 
+      }
+    })
+
+  }, (err) => {
+    res.json({ id: "Error in database connection", error: err });
+  })
+});
+
 
 module.exports = router;
