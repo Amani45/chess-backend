112a445c58c768e817bb144d0bd997dcaa2a30a8
Amani <am.m4@hotmail.com>
2020-04-12 20:52:54 +0300

    adding open end point

--- myapp/routes/api.js
+++ myapp/routes/api.js
@@ -23,6 +23,16 @@ router.get("/ping", function (req, res, next) {
   res.json({ success: true, message: "Ping Ping!" })
 });
 
+router.get("/open", function (req, res, next) {
+  if (LED.readSync() === 0) { //check the pin state, if the state is 0 (or off)
+    LED.writeSync(1); //set pin state to 1 (turn LED on)
+    setTimeout( ()=>{
+      LED.writeSync(0); //set pin state to 0 (turn LED off)
+    }, 1000)
+  } 
+  res.json({ success: true, message: "Sending Open Single!" })
+});
+
 
 /**
  * Find All Users
