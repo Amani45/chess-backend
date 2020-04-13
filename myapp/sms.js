378958d26df687cb13122607d93542c914e11ddd
Amani <am.m4@hotmail.com>
2020-04-13 17:21:27 +0300

    adding updated auth token

--- myapp/sms.js
+++ myapp/sms.js
@@ -1,5 +1,5 @@
 const accountSid = 'AC5269676b8afff87a0c53a0d71f618e2b';
-const authToken = 'ce02dca980452d085de45609c349063f';
+const authToken = '1fd3bd5e5688c0c868826c0839511b52';
 const client = require('twilio')(accountSid, authToken);
 
 function sendSMS(to,code) {
